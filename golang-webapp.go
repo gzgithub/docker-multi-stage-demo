4357e586fbd77a0fd1c92422adc7fc6f44a1d259
GZ <gzgithub@users.noreply.github.com>
2018-04-20 18:04:23 +0800

    Update golang-webapp.go
    serving a static page

--- golang-webapp.go
+++ golang-webapp.go
@@ -6,14 +6,16 @@ import (
    "os"
 )
 
+/*
 func handler(w http.ResponseWriter, r *http.Request) {
     var name, _ = os.Hostname()
 
     fmt.Fprintf(w, "<h1>This request was processed by host: %s</h1>\n", name)
 }
+*/
 
 func main() {
     fmt.Fprintf(os.Stdout, "Golang-based web server started. Listening on 0.0.0.0:80\n")
-    http.HandleFunc("/", handler)
+    http.Handle("/", http.FileServer(http.Dir("./static_pages")))
     http.ListenAndServe(":80", nil)
 }
